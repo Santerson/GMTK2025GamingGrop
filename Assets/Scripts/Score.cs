77f12392eeb7ea8215b1a8b6755c59c8bb3db667
ggiacardo <162412057+ggiacardo@users.noreply.github.com>
2025-08-01 21:24:57 +0800

    made a highscore system. Idk if it works tho

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,26 +5,49 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-
     [SerializeField] TextMeshProUGUI text = null;
+    [SerializeField] TextMeshProUGUI highscoreText = null; // Optional - can be null
     [SerializeField] int phaseThreshold = 10;
     [SerializeField] int phaseIncreaseRate = 10;
     [SerializeField] GameObject phaseIncreaseEffect = null;
-
     [SerializeField] LineRenderer progressBar;
     [SerializeField] AudioSource nextStageSFX;
+    [SerializeField] AudioSource newHighscoreSFX; // Optional - can be null
 
     int pastPhaseThresholdCount = 0;
-
     float score = 0;
     float progressToNextPhase = 0;
+    float highscore = 0;
+    bool isNewHighscore = false;
 
-    // Update is called once per frame
+    void Start()
+    {
+        // Load the saved highscore when the game starts
+        highscore = PlayerPrefs.GetFloat("Highscore", 0);
+        UpdateHighscoreText();
+    }
+
     void Update()
     {
         score += Time.deltaTime;
         progressToNextPhase += Time.deltaTime;
         text.text = "Time: " + (int)score;
+
+        // Check for new highscore during gameplay
+        if (score > highscore)
+        {
+            if (!isNewHighscore)
+            {
+                isNewHighscore = true;
+                if (newHighscoreSFX != null) // Null check for SFX
+                {
+                    newHighscoreSFX.Play();
+                }
+            }
+            highscore = score;
+            UpdateHighscoreText();
+        }
+
         if ((int)progressToNextPhase >= phaseThreshold)
         {
             FindObjectOfType<ObstacleGenerator>().RaisePhase();
@@ -33,9 +56,24 @@ public class Score : MonoBehaviour
             Instantiate(phaseIncreaseEffect, Vector2.zero, Quaternion.identity);
             nextStageSFX.Play();
         }
-        progressBar.SetPosition(0, new Vector2(-9f,0f));
-        float progress = Mathf.Clamp01(progressToNextPhase / phaseThreshold);
 
+        progressBar.SetPosition(0, new Vector2(-9f, 0f));
+        float progress = Mathf.Clamp01(progressToNextPhase / phaseThreshold);
         progressBar.SetPosition(1, new Vector2(Mathf.Lerp(-9, 9, progress), 0));
+    }
+
+    void UpdateHighscoreText()
+    {
+        if (highscoreText != null) // Null check for highscore text
+        {
+            highscoreText.text = "Best: " + (int)highscore;
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Save the highscore when the game ends
+        PlayerPrefs.SetFloat("Highscore", highscore);
+        PlayerPrefs.Save();
     }
 }
